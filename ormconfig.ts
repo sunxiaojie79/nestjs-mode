54aee34b6c2f08c8eb6faf9b54130f630ac426e9
sunxiaojie79 <297280911@qq.com>
2025-05-08 23:47:50 +0800

    feat: database config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,21 +1,40 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { User } from './src/user/user.entity';
-import { Profile } from './src/user/profile.entity';
-import { Logs } from './src/logs/logs.entity';
-import { Roles } from './src/roles/roles.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
-export const connectionParams = {
-  type: 'mysql',
-  host: '127.0.0.1',
-  port: 3306,
-  username: 'root',
-  password: 'example',
-  database: 'testdb',
-  entities: [User, Profile, Logs, Roles],
-  synchronize: true,
-  // logging: process.env.NODE_ENV === 'development',
-  logging: false,
-} as TypeOrmModuleOptions;
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import { ConfigEnum } from './src/enum/config.enum';
+// 通过环境变量读取不同的.env文件
+function getEnv(env: string): Record<string, unknown> {
+  if (fs.existsSync(env)) {
+    return dotenv.parse(fs.readFileSync(env));
+  }
+  return {};
+}
+
+// 通过dotEnv解析不同的配置
+function buildConnectionOptions() {
+  const defaultConfig = getEnv('.env');
+  const envConfig = getEnv(`.env.${process.env.NODE_ENV}`);
+  const config = { ...defaultConfig, ...envConfig };
+
+  const entitiesDir =
+    process.env.NODE_ENV === 'test'
+      ? [__dirname + '/**/*.entity.ts']
+      : [__dirname + '/**/*.entity{.ts,.js}'];
+  return {
+    type: config[ConfigEnum.DB_TYPE],
+    host: config[ConfigEnum.DB_HOST],
+    port: config[ConfigEnum.DB_PORT],
+    username: config[ConfigEnum.DB_USERNAME],
+    password: config[ConfigEnum.DB_PASSWORD],
+    database: config[ConfigEnum.DB_DATABASE],
+    entities: entitiesDir,
+    synchronize: true,
+    // logging: process.env.NODE_ENV === 'development',
+    logging: false,
+  } as TypeOrmModuleOptions;
+}
+export const connectionParams = buildConnectionOptions();
 
 export default new DataSource({
   ...connectionParams,
--- src/logs/logs.module.ts
+++ src/logs/logs.module.ts
@@ -4,6 +4,19 @@ import { ConfigService } from '@nestjs/config';
 import { format, transports } from 'winston';
 import 'winston-daily-rotate-file';
 import { LogEnum } from '../enum/config.enum';
+
+function createDailyRotateTransport(level: string, filename: string) {
+  return new transports.DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${filename}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD-HH',
+    maxSize: '20m',
+    maxFiles: '14d',
+    zippedArchive: true,
+    format: format.combine(format.timestamp(), format.simple()),
+  });
+}
 @Module({
   imports: [
     WinstonModule.forRootAsync({
@@ -17,37 +30,20 @@ import { LogEnum } from '../enum/config.enum';
           ),
         });
 
-        const dailyTransport = new transports.DailyRotateFile({
-          level: configService.get(LogEnum.LOG_LEVEL),
-          dirname: 'logs',
-          filename: 'app-%DATE%.log',
-          datePattern: 'YYYY-MM-DD-HH',
-          maxSize: '20m',
-          maxFiles: '14d',
-          zippedArchive: true,
-          format: format.combine(format.timestamp(), format.simple()),
-        });
         console.log(
           '🚀 ~ configService.get(LogEnum.LOG_LEVEL):',
           configService.get(LogEnum.LOG_LEVEL),
           configService.get(LogEnum.LOG_ON),
         );
 
-        const dailyInfoTransport = new transports.DailyRotateFile({
-          level: configService.get(LogEnum.LOG_LEVEL),
-          dirname: 'logs',
-          filename: 'info-%DATE%.log',
-          datePattern: 'YYYY-MM-DD-HH',
-          maxSize: '20m',
-          maxFiles: '14d',
-          zippedArchive: true,
-          format: format.combine(format.timestamp(), format.simple()),
-        });
         return {
           transports: [
             consoleTransport,
             ...(configService.get(LogEnum.LOG_ON)
-              ? [dailyTransport, dailyInfoTransport]
+              ? [
+                  createDailyRotateTransport('info', 'app'),
+                  createDailyRotateTransport('warn', 'error'),
+                ]
               : []),
           ],
         } as WinstonModuleOptions;
